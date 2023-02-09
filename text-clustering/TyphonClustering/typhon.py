2ac55a645185f8ded0a9f573f49aef1ce4696b05
slothPete7773 <Veerakit413@gmail.com>
2023-02-09 13:29:16 +0700

    refactor(but not better)

--- text-clustering/TyphonClustering/typhon.py
+++ text-clustering/TyphonClustering/typhon.py
@@ -58,13 +58,33 @@ class Typhon:
             text = re.sub(r'https?://\S+', "", text)
             text = " ".join(re.sub(r'https?://\S+', "", text).split())
             return text
+        
+        res_list = []
+        for md in list_mds:
+            processing_text = str(md)
+            processing_text = processing_text.lower()
+            # print(f'{processing_text}\n============================')
+            processing_text = re.sub(r'\\n', r'', processing_text) # Remove \n from the text, it is not '\n' it is actually '\\n', gave me headache.
+            processing_text = re.sub(f"[{re.escape(string.punctuation)}]", " ", processing_text).strip() # Remove punctuations
+            processing_text = " ".join(processing_text.split()) # Remove excess spaces
+            processing_text = remove_hyperlink(processing_text) # Remove hyperlink in text
+            processing_text = re.sub(r"<.*?>", " ", processing_text) # Remove tags (<...>) in text
+            processing_text = re.sub(r'[^a-zA-Z0-9\s]', "", processing_text) # Remove non-alphabetical letters
 
-        def remove_tags(text):
-            return re.sub(r"<.*?>", " ", text)
-
+            processing_tokens = nltk.word_tokenize(processing_text)
+            # print(f'{processing_tokens}\n=========================')
+            processing_tokens = [token for token in processing_tokens if token not in stopwords]
+            processing_tokens = [self.porter_stemmer.stem(token) for token in processing_tokens]
+            processing_tokens = [self.wordnet_lemmatizer.lemmatize(token) for token in processing_tokens]
+            # print(processing_tokens)
+            processed_text = " ".join(processing_tokens)
+            # print(processed_text)
+            res_list.append(processed_text)
         if filter_result:
             res_list = list(filter(None, res_list)) # Filter out the empty value
 
+        return res_list
+    
     def generate_embedding(self, markdown_list):
         print(type(markdown_list))
         tokens_ids = self.model.tokenize(markdown_list,max_length=512,mode="<encoder-only>",padding=True)
