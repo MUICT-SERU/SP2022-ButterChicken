63862daa2d1e41a9c36b335902a77bc672b3f740
Paphon <Paphon_work@hotmail.com>
2023-02-25 15:51:18 +0700

    add preprocess and embedding api

--- text-clustering/server/server.py
+++ text-clustering/server/server.py
@@ -9,13 +9,17 @@ number = 3
 async def root():
     return {"message": "Hello World"}
 
-@app.get("/preprocess")
-async def preprocess():
-    model.preprocess()
-    return {"message": "Hello World"}
+@app.get("/preprocess/{markdown}")
+def preprocess(markdown):
+    mk = [markdown]
+    data = model.preprocess(mk)
+    print(data)
+    return data[0]
 
 
-@app.get("/embedding")
-async def embedding():
-    model.generate_embedding()
-    return {"message": "Hello World"}
+@app.get("/embedding/{markdown}")
+def embedding(markdown):
+    mk = [markdown]
+    data = model.generate_embedding(mk)
+    print(data)
+    return data[0]
